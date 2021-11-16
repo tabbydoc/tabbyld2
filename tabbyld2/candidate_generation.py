cac26d4a8d890c4f91472b82853dec1402a02b43
LedZeppe1in <tualatin32@mail.ru>
2021-11-16 22:23:31 +0800

    Add union_candidate_entity_lists function and update generate_candidate_entities function

--- tabbyld2/candidate_generation.py
+++ tabbyld2/candidate_generation.py
@@ -1,25 +1,21 @@
 import tabbyld2.dbpedia_lookup as dbl
-import tabbyld2.graphql_interface as gql
+import tabbyld2.dbpedia_sparql_endpoint as dbs
 
 
-def generate_candidate_classes(class_mention):
+def union_candidate_entity_lists(candidate_entities_from_sparql_endpoint, candidate_entities_from_dbl):
     """
-    Генерация классов кандидатов на основе текстового упоминания класса.
-    :param class_mention: текстовое упоминание класса
-    :return: словарь классов кандидатов для упоминания класса
+    Объединение двух наборов сущностей кандидатов, полученных от конечной точки DBpedia SPARQL Endpoint и
+    сервиса DBpedia Lookup.
+    :param candidate_entities_from_sparql_endpoint: набор сущностей кандидатов, полученный от DBpedia SPARQL Endpoint
+    :param candidate_entities_from_dbl: набор сущностей кандидатов, полученный от сервиса DBpedia Lookup
+    :return: объединенный отсортированный список сущностей кандидатов без дубликатов
     """
-    result_list = dict()
-    # Получение типов концептов (классов) из БЗ Talisman
-    candidate_classes = gql.get_concept_types()
-    if candidate_classes:
-        item = []
-        for candidate_class in candidate_classes:
-            item.append(candidate_class["name"])
-        result_list[class_mention] = item
-    else:
-        result_list[class_mention] = []
+    ts = set()
+    rm_duplicate = lambda l: [x for x in l if not (x in ts or ts.add(x))]
+    candidate_entities = rm_duplicate(candidate_entities_from_sparql_endpoint) + [i for i in rm_duplicate(
+        candidate_entities_from_dbl) if i not in candidate_entities_from_sparql_endpoint]
 
-    return result_list
+    return candidate_entities
 
 
 def generate_candidate_entities(entity_mention):
@@ -29,12 +25,33 @@ def generate_candidate_entities(entity_mention):
     :return: словарь сущностей кандидатов для упоминания сущности
     """
     result_list = dict()
+    # Получение сущностей кандидатов на основе конечной точки DBpedia SPARQL Endpoint
+    candidate_entities_from_sparql_endpoint = dbs.get_entities(entity_mention, False)
     # Получение сущностей кандидатов от сервиса DBpedia Lookup
-    candidate_entities = dbl.get_entities(entity_mention, 10, None, True)
+    candidate_entities_from_dbl = dbl.get_entities(entity_mention, 100, None, False)
+    # Получение объединенного набора (списка) сущностей кандидатов
+    candidate_entities = union_candidate_entity_lists(candidate_entities_from_sparql_endpoint,
+                                                      candidate_entities_from_dbl)
     if candidate_entities:
         result_list[entity_mention] = candidate_entities
     else:
         result_list[entity_mention] = []
+
+    return result_list
+
+
+def generate_candidate_classes(class_mention):
+    """
+    Генерация классов кандидатов на основе текстового упоминания класса.
+    :param class_mention: текстовое упоминание класса
+    :return: словарь классов кандидатов для упоминания класса
+    """
+    result_list = dict()
+    candidate_classes = []
+    if candidate_classes:
+        result_list[class_mention] = candidate_classes
+    else:
+        result_list[class_mention] = []
 
     return result_list
 
@@ -46,8 +63,7 @@ def generate_candidate_properties(class_mention):
     :return: словарь свойств кандидатов
     """
     result_list = dict()
-    # Получение типов связей (отношений между классами) из БЗ Talisman
-    candidate_properties = gql.get_concept_link_types()
+    candidate_properties = []
     if candidate_properties:
         result_list[class_mention] = candidate_properties
     else:
