6eb4a9b404005d700d360149e27a3fe2170d7bf3
LedZeppe1in <tualatin32@mail.ru>
2022-01-27 11:20:39 +0800

    Translated comments for main.py

--- tabbyld2/main.py
+++ tabbyld2/main.py
@@ -40,148 +40,143 @@ ANNOTATED_COLUMN_DATA_PATH = "/annotated_column_data/"
 ANNOTATED_PROPERTY_DATA_PATH = "/annotated_property_data/"
 
 if __name__ == '__main__':
-    # Сохранение набора электронных таблиц в формате json
+    # Saving a set of source tables in the json format
     pr.save_json_dataset(CSV_FILE_PATH, JSON_FILE_PATH)
 
-    # Обход файлов json-файлов электронных таблиц в каталоге
     for root, dirs, files in os.walk(JSON_FILE_PATH):
         for file in files:
             if utl.allowed_file(file, {"json"}):
-                # Очистка данных
+                # Data cleaning
                 cleared_data = cln.clean(JSON_FILE_PATH + file)
                 if cleared_data:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + CLEARED_DATA_PATH
-                    # Проверка существования каталога для сохранения результатов очистки данных
+                    # Checking the existence of path to save data cleaning results
                     utl.check_directory(path)
-                    # Запись json-файла c очищенными табличными данными
+                    # Writing json file with cleared tabular data
                     with open(path + file, "w") as outfile:
                         json.dump(cleared_data, outfile, indent=4)
 
-                # Распознавание именованных сущностей в исходном словаре (таблице)
+                # Recognition of named entities in source table (dict)
                 recognized_data = cc.recognize_named_entities(cleared_data)
                 if recognized_data:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RECOGNIZED_DATA_PATH
-                    # Проверка существования каталога для сохранения результатов распознавания именованных сущностей
+                    # Checking the existence of path to save named entity recognition results
                     utl.check_directory(path)
-                    # Запись json-файла c результатами распознавания именованных сущностей
+                    # Writing json file with named entity recognition results
                     with open(path + file, "w") as outfile:
                         json.dump(recognized_data, outfile, indent=4)
 
-                # Классификация столбцов в исходном словаре (таблице) на основе распознанных именованных сущностей
+                # Classification of columns in source table (dict) based on recognized named entities
                 classified_data = cc.classify_recognized_named_entities(recognized_data)
                 classified_data = cc.classify_columns(classified_data)
-                # Определение сущностного (тематического) столбца
+                # Identifying a subject column among categorical columns
                 classified_data = cc.define_subject_column(cleared_data, classified_data)
                 if classified_data:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + CLASSIFIED_DATA_PATH
-                    # Проверка существования каталога для сохранения результатов классификации столбцов
+                    # Checking the existence of path to save column classification results
                     utl.check_directory(path)
-                    # Запись json-файла c результатами классификации столбцов
+                    # Writing json file with named column classification results
                     with open(path + file, "w") as outfile:
                         json.dump(classified_data, outfile, indent=4)
 
                 print("*********************************************************")
-                # Поиск сущностей кандидатов для всех ячеек категорильных столбцов
+
+                # Finding candidate entities for all cells of categorical columns including a subject column
                 candidate_entity_data = cg.get_candidate_entities_for_table(cleared_data, classified_data)
                 if candidate_entity_data:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + CANDIDATE_ENTITIES_PATH
-                    # Проверка существования каталога для сохранения результатов поиска сущностей кандидатов для таблицы
+                    # Checking the existence of path to save candidate entity lookup results
                     utl.check_directory(path)
-                    # Запись json-файла c результатами поиска сущностей кандидатов
+                    # Writing json file with candidate entity lookup results
                     with open(path + file, "w") as outfile:
                         json.dump(candidate_entity_data, outfile, indent=4)
 
                 print("*********************************************************")
-                # Ранжирование сущностей кандидатов по схоству строк
+
+                # Ranking candidate entities by string similarity and
+                # writing json file with candidate entity ranking results
                 ranked_candidate_entities_by_ss = ant.ranking_candidate_entities_by_ss(candidate_entity_data)
                 if ranked_candidate_entities_by_ss:
-                    # Формирование пути к файлу
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES_BY_SS
-                    # Проверка существования каталога для сохранения результатов ранжирования сущностей кандидатов
                     utl.check_directory(path)
-                    # Запись json-файла c результатами ранжирования сущностей кандидатов
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_candidate_entities_by_ss, outfile, indent=4)
-                # Ранжирование сущностей кандидатов по сходству на основе NER-классов
+
+                # Ranking candidate entities by NER based similarity and
+                # writing json file with candidate entity ranking results
                 ranked_candidate_entities_by_ns = ant.ranking_candidate_entities_by_ns(candidate_entity_data,
                                                                                        recognized_data)
                 if ranked_candidate_entities_by_ns:
-                    # Формирование пути к файлу
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES_BY_NS
-                    # Проверка существования каталога для сохранения результатов ранжирования сущностей кандидатов
                     utl.check_directory(path)
-                    # Запись json-файла c результатами ранжирования сущностей кандидатов
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_candidate_entities_by_ns, outfile, indent=4)
-                # Ранжирование сущностей кандидатов категориальных столбцов (включая сущностный столбец) по
-                # сходству на основе заголовка столбца
+
+                # Ranking candidate entities by heading based similarity and
+                # writing json file with candidate entity ranking results
                 ranked_candidate_entities_by_hs = ant.ranking_candidate_entities_by_hs(candidate_entity_data)
                 if ranked_candidate_entities_by_hs:
-                    # Формирование пути к файлу
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES_BY_HS
-                    # Проверка существования каталога для сохранения результатов ранжирования сущностей кандидатов
                     utl.check_directory(path)
-                    # Запись json-файла c результатами ранжирования сущностей кандидатов
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_candidate_entities_by_hs, outfile, indent=4)
-                # Ранжирование сущностей кандидатов по сходству на основе семантической близости между сущностями кандидатами
+
+                # Ranking candidate entities by entity embeddings based semantic similarity and
+                # writing json file with candidate entity ranking results
                 ranked_candidate_entities_by_ess = ant.ranking_candidate_entities_by_ess(candidate_entity_data)
                 if ranked_candidate_entities_by_ess:
-                    # Формирование пути к файлу
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES_BY_ESS
-                    # Проверка существования каталога для сохранения результатов ранжирования сущностей кандидатов
                     utl.check_directory(path)
-                    # Запись json-файла c результатами ранжирования сущностей кандидатов
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_candidate_entities_by_ess, outfile, indent=4)
-                # Ранжирование сущностей кандидатов по сходству на основе контекста
+
+                # Ranking candidate entities by context based similarity and
+                # writing json file with candidate entity ranking results
                 ranked_candidate_entities_by_cs = ant.ranking_candidate_entities_by_cs(candidate_entity_data)
                 if ranked_candidate_entities_by_cs:
-                    # Формирование пути к файлу
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES_BY_CS
-                    # Проверка существования каталога для сохранения результатов ранжирования сущностей кандидатов
                     utl.check_directory(path)
-                    # Запись json-файла c результатами ранжирования сущностей кандидатов
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_candidate_entities_by_cs, outfile, indent=4)
-                # Агрегирование оценок (рангов) для значений ячеек, полученных на основе пяти эвристик
-                final_ranked_candidate_entities = ant.aggregate_ranked_candidate_entities(ranked_candidate_entities_by_ss,
-                                                                                          ranked_candidate_entities_by_ns,
-                                                                                          ranked_candidate_entities_by_hs,
-                                                                                          ranked_candidate_entities_by_ess,
-                                                                                          ranked_candidate_entities_by_cs)
+
+                # Aggregating scores (ranks) for cell values (mentions) obtained based on five heuristics
+                final_ranked_candidate_entities = ant.aggregate_ranked_candidate_entities(
+                    ranked_candidate_entities_by_ss, ranked_candidate_entities_by_ns, ranked_candidate_entities_by_hs,
+                    ranked_candidate_entities_by_ess, ranked_candidate_entities_by_cs)
                 if final_ranked_candidate_entities:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_ENTITIES
-                    # Проверка существования каталога для сохранения результатов объединения рангов для сущностей кандидатов
+                    # Checking the existence of path to save results of aggregated scores for candidate entities
                     utl.check_directory(path)
-                    # Запись json-файла c результатами итогового ранжирования сущностей кандидатов
+                    # Writing json file with finally ranking results for candidate entities
                     with open(path + file, "w") as outfile:
                         json.dump(final_ranked_candidate_entities, outfile, indent=4)
-                # Аннотирование значений ячеек на основе ранжированного списка сущностей кандидатов
+
+                # Annotating cell values (mentions) based on ranked list of candidate entities
                 annotated_cell_data = ant.annotate_cells(final_ranked_candidate_entities)
                 if annotated_cell_data:
-                    print("Ячейки для '" + file + "' проаннотированы")
-                    # Формирование пути к файлу
+                    print("Cells for '" + file + "' are annotated.")
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + ANNOTATED_CELL_DATA_PATH
-                    # Проверка существования каталога для сохранения результатов аннотирования значений ячеек
+                    # Checking the existence of path to save results of annotating cell values
                     utl.check_directory(path)
-                    # Запись json-файла c результатами аннотирования значений ячеек
+                    # Writing json file with results of annotating cell values
                     with open(path + file, "w") as outfile:
                         json.dump(annotated_cell_data, outfile, indent=4)
 
                 print("*********************************************************")
-                # Ранжирование классов кандидатов для категориальных столбцов по сходству голосования большинством
+
+                # Ranking candidate classes for categorical columns by majority voting method
                 ranked_classes_by_mv = ant.ranking_candidate_classes_by_mv(annotated_cell_data, classified_data)
                 if ranked_classes_by_mv:
-                    # Формирование пути к файлу
+                    # Forming path to file
                     path = PROVENANCE_PATH + utl.remove_suffix_in_filename(file) + RANKED_CANDIDATE_CLASSES_BY_MV
-                    # Проверка существования каталога для сохранения результатов ранжирования классов
+                    # Checking for existence of path to save class ranking results
                     utl.check_directory(path)
-                    # Запись json-файла c результатами итогового ранжирования классов
+                    # Writing json file with class ranking results
                     with open(path + file, "w") as outfile:
                         json.dump(ranked_classes_by_mv, outfile, indent=4)
