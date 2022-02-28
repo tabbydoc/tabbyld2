091560121193bbe11b426ffd870c111e906caff4
LedZeppe1in <tualatin32@mail.ru>
2022-02-28 16:13:00 +0800

    Added check_letter_and_digit_existence function to cleaner.py and updated remove_garbage_characters function in cleaner.py

--- tabbyld2/cleaner.py
+++ tabbyld2/cleaner.py
@@ -12,27 +12,44 @@ def remove_multiple_spaces(text):
     return new_text
 
 
+def check_letter_and_digit_existence(text):
+    """
+    Проверка существования в исходном тексте (строке) букв и(или) цифр.
+    :param text: исходный текст
+    :return: флаг, показывающий отсутствие букв и цифр в строке
+    """
+    exist_letter = True if any(map(str.isalpha, text)) else False
+    exist_digit = True if any(map(str.isdigit, text)) else False
+
+    return True if not exist_letter and not exist_digit else False
+
+
 def remove_garbage_characters(text):
     """
-    Удаление "мусорных" символов в строке, которая не содержит буквы или цифры.
+    Удаление "мусорных" символов в строке.
     :param text: исходный текст
     :return: текст с удаленными "мусорными" символами
     """
-    letter_exist = False
-    if any(map(str.isalpha, text)):
-        letter_exist = True
-    digit_exist = False
-    if any(map(str.isdigit, text)):
-        digit_exist = True
-    if letter_exist is False and digit_exist is False:
+    # Удаление "мусорных" символов в строке, которая не содержит буквы или цифры
+    if check_letter_and_digit_existence(text):
         text = ""
-    # if text != "":
-    #     symbols = ["?", "!", "@", "#", "$", "%", "^", "&", "*", "+", "=", "{", "}", "[", "]", ":", ";" "<", ">"]
-    #     new_text = text
-    #     for symbol in symbols:
-    #         new_text = new_text.replace(symbol, "")
 
-    return text
+    new_text = text
+    if text:
+        # Удаление "мусорных" символов в строке
+        # for symbol in symbols:
+        #     new_text = new_text.replace(symbol, "")
+        # Удаление "мусорных" символов в строке, которые отделены пробелами
+        word_list = text.split()
+        new_text = ""
+        for word in word_list:
+            if not check_letter_and_digit_existence(word) and word:
+                if new_text:
+                    new_text += " " + word
+                else:
+                    new_text = word
+
+    return new_text
 
 
 def fix_text(value):
