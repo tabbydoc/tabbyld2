5c92ca19d25ff939fbb64794559a91c4c63e7e3a
LedZeppe1in <tualatin32@mail.ru>
2022-02-28 16:16:41 +0800

    Updated convert_t2dv2_tables_to_csv function in parser.py

--- tabbyld2/parser.py
+++ tabbyld2/parser.py
@@ -2,7 +2,7 @@ import json
 import os
 import pandas as pd
 import tabbyld2.utility as ut
-from tabbyld2.config import ResultPath
+from tabbyld2.config import ResultPath, EvaluationPath
 from tabbyld2.utility import allowed_file
 
 
@@ -98,28 +98,25 @@ def convert_t2dv2_tables_to_csv():
     Конвертация json-файлов представления электронных таблиц из набора данных T2Dv2 в формат csv.
     """
     # Cycle through table files
-    for root, dirs, files in os.walk(ResultPath.T2DV2_JSON_PATH):
+    for root, dirs, files in os.walk(EvaluationPath.T2DV2_JSON):
         for file in files:
             if allowed_file(file, {"json"}):
-                try:
-                    # Extract table from source json file
-                    with open(ResultPath.T2DV2_JSON_PATH + file, "r", errors="ignore") as fp:
-                        source_json_data = json.load(fp)
-                        table = dict()
-                        for key, items in source_json_data.items():
-                            if key == "relation":
-                                for item in items:
-                                    index = 0
-                                    cells = []
-                                    for elem in item:
-                                        if index != 0:
-                                            cells.append(elem)
-                                    table[item[0]] = item[1:]
-                        # Form name for new csv file
-                        csv_file_name = os.path.splitext(file)[0] + ".csv"
-                        # Save new csv file for tabular data
-                        data_frame = pd.DataFrame(table)
-                        data_frame.to_csv(ResultPath.CSV_FILE_PATH + csv_file_name, header=True, index=False)
-                        print("File '" + str(csv_file_name) + "' is created successfully.")
-                except json.decoder.JSONDecodeError:
-                    print("Error decoding json table file!")
+                print(file)
+                # Extract table from source json file
+                source_json_data = pd.read_json(EvaluationPath.T2DV2_JSON + file, encoding="unicode_escape")
+                table = dict()
+                for key, items in source_json_data.items():
+                    if key == "relation":
+                        index = 0
+                        for item in items:
+                            if item[0]:
+                                table[item[0]] = item[1:]
+                            else:
+                                table[index] = item[1:]
+                            index += 1
+                # Form name for new csv file
+                csv_file_name = os.path.splitext(file)[0] + ".csv"
+                # Save new csv file for tabular data
+                data_frame = pd.DataFrame(table)
+                data_frame.to_csv(ResultPath.CSV_FILE_PATH + csv_file_name, header=True, index=False)
+                print("File '" + str(csv_file_name) + "' is created successfully.")
