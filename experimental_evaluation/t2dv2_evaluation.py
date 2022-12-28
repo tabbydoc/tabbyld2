b062de15b5193ea2abe189bdc7c06b29eaa6d276
LedZeppe1in <tualatin32@mail.ru>
2022-12-28 19:33:46 +0800

    T2Dv2 evaluation refactoring

--- experimental_evaluation/t2dv2_evaluation.py
+++ experimental_evaluation/t2dv2_evaluation.py
@@ -1,7 +1,9 @@
-import html
+import csv
 import json
 import os
-import urllib.parse
+from urllib.parse import unquote
+from html import unescape
+
 import pandas as pd
 
 from datetime import datetime
@@ -10,7 +12,7 @@ import stanza
 from duckling import DucklingWrapper
 
 import tabbyld2.pipeline as pl
-from experimental_evaluation.evaluation_model import AdditionalEvaluation, MainEvaluation, TableEvaluation
+from experimental_evaluation.evaluation_model import TableEvaluation
 
 from tabbyld2.helpers.file import allowed_file, remove_suffix_in_filename, write_json_file
 from tabbyld2.helpers.parser import deserialize_table, save_json_dataset
@@ -20,101 +22,68 @@ from tabbyld2.preprocessing.atomic_column_classifier import ColumnType
 
 class T2Dv2TableEvaluation(TableEvaluation):
     """
-    Experimental evaluations for table from T2Dv2 dataset.
+    Experimental evaluations for table from T2Dv2 dataset
     """
 
     def evaluate_subject_column_identification(self):
         """
-        Evaluate subject column identification among categorical columns.
+        Evaluate subject column identification among categorical columns
         """
-        # Get class checked data for tables from T2Dv2 dataset
         checked_data = pd.DataFrame(pd.read_csv(EvaluationPath.T2DV2 + EvaluationPath.T2DV2_CLASS_CHECKED,
                                                 sep=",", header=None, index_col=False))
-        exist_subject_column, rows, column_indices = False, [], {}
-        for key, items in checked_data.items():
-            if key == 0:
-                for i in range(len(items)):
-                    if items[i] == self.table.table_name:
-                        rows.append(i)
-            if key == 1:
-                for i in range(len(items)):
-                    for row_index in rows:
-                        if i == row_index:
-                            column_indices[row_index] = items[i]
-            if key == 2:
-                for i in range(len(items)):
-                    for row_index in rows:
-                        if i == row_index:
-                            for row, column_index in column_indices.items():
-                                for k in range(len(self.table.columns)):
-                                    if i == row and int(column_index) == k:
-                                        if items[i] and self.table.columns[k].column_type == ColumnType.SUBJECT_COLUMN:
-                                            exist_subject_column = True
-        # Calculate evaluations
-        self._subject_column_identification_evaluation = MainEvaluation()
-        self._subject_column_identification_evaluation._precision = 1.0 if exist_subject_column else 0.0
+        exist_subject_column = False
+        for i in range(len(checked_data.get(0))):
+            if checked_data.get(0)[i] == self.table.table_name:
+                for j in range(len(self.table.columns)):
+                    if self.table.columns[j].column_type == ColumnType.SUBJECT_COLUMN and j == int(checked_data.get(1)[i]) and \
+                            checked_data.get(2)[i]:
+                        exist_subject_column = True
+        self.subject_column_identification_evaluation.set_precision(1.0 if exist_subject_column else 0.0)
         exist_subject_column = False
         for column in self.table.columns:
             if column.column_type == ColumnType.SUBJECT_COLUMN:
                 exist_subject_column = True
-        self._subject_column_identification_evaluation._recall = 1.0 if exist_subject_column else 0.0
-        self._subject_column_identification_evaluation.calculate_f1_score()
+        self.subject_column_identification_evaluation.set_recall(1.0 if exist_subject_column else 0.0)
+        self.subject_column_identification_evaluation.calculate_f1_score()
 
     def evaluate_cell_entity_annotation(self):
         """
-        Evaluate cell entity annotation (CEA) task.
+        Evaluate cell entity annotation (CEA) task
         """
-        for _, _, instance_files in os.walk(EvaluationPath.T2DV2_INSTANCE):
-            for instance_file in instance_files:
-                if remove_suffix_in_filename(instance_file) == self.table.table_name:
-                    # Get checked tabular data
-                    instance_data = pd.DataFrame(pd.read_csv(EvaluationPath.T2DV2_INSTANCE + instance_file, sep=",",
-                                                             header=None, index_col=False, encoding="unicode_escape"))
-                    # Annotation comparison
-                    correctly_annotated_cells, annotated_cells, cell_number, instances, values = 0, 0, 0, [], []
-                    for key, items in instance_data.items():
-                        if key == 0:
-                            instances, cell_number = items, len(items)
-                        if key == 1:
-                            values = items
-                        if key == 2:
-                            for column in self.table.columns:
-                                if column.column_type == ColumnType.SUBJECT_COLUMN:
-                                    for i in range(len(column.cells)):
-                                        if column.cells[i].source_value is not None:
-                                            for j in range(len(items)):
-                                                if (i + 1) == int(items[j]):
-                                                    if html.unescape(values[j].lower()) == \
-                                                            column.cells[i].source_value.lower():
-                                                        if column.cells[i].annotation is not None:
-                                                            annotated_cells += 1
-                                                            if urllib.parse.unquote(instances[j].lower()) == \
-                                                                    column.cells[i].annotation.lower() or instances[j].encode("raw_unicode_escape").decode("utf-8").lower() == column.cells[i].annotation.lower():
-                                                                correctly_annotated_cells += 1
-                                                            else:
-                                                                print("INS: " + urllib.parse.unquote(instances[j]) +
-                                                                      " != ANN: " + column.cells[i].annotation)
-                                                        else:
-                                                            print("INS: " + urllib.parse.unquote(instances[j]) +
-                                                                  " != ANN: NULL")
-                    # Calculate evaluations
-                    self._cell_entity_annotation_evaluation = MainEvaluation()
-                    self._cell_entity_annotation_evaluation._precision = correctly_annotated_cells / annotated_cells if annotated_cells != 0 else 0
-                    self._cell_entity_annotation_evaluation._recall = correctly_annotated_cells / cell_number if cell_number != 0 else 0
-                    self._cell_entity_annotation_evaluation.calculate_f1_score()
-                    print("***************************************************")
-                    print("Correctly annotated cells = " + str(correctly_annotated_cells))
-                    print("Annotated cells = " + str(annotated_cells))
-                    print("Cell number = " + str(cell_number))
+        correctly_annotated_cells, annotated_cells, cell_number = 0, 0, 0
+        with open(EvaluationPath.T2DV2_INSTANCE + self.table.table_name + ".csv", "r", newline="", encoding="utf-8") as file:
+            for (uri, cell_value, _) in csv.reader(file):
+                cell_number += 1
+                for column in self.table.columns:
+                    if column.column_type == ColumnType.SUBJECT_COLUMN:
+                        for i in range(len(column.cells)):
+                            if column.cells[i].source_value is not None and unescape(cell_value.lower()) == \
+                                    column.cells[i].source_value.lower():
+                                if column.cells[i].annotation is not None:
+                                    annotated_cells += 1
+                                    if unquote(uri.lower()) == column.cells[i].annotation.lower() or \
+                                            uri.encode("raw_unicode_escape").decode("utf-8").lower() == column.cells[i].annotation.lower():
+                                        correctly_annotated_cells += 1
+                                    else:
+                                        print("INS: " + unquote(uri) + " != ANN: " + column.cells[i].annotation)
+                                else:
+                                    print("INS: " + unquote(uri) + " != ANN: NULL")
+        self.cell_entity_annotation_evaluation.set_precision(correctly_annotated_cells / annotated_cells if annotated_cells != 0 else 0)
+        self.cell_entity_annotation_evaluation.set_recall(correctly_annotated_cells / cell_number if cell_number != 0 else 0)
+        self.cell_entity_annotation_evaluation.calculate_f1_score()
+        print("***************************************************")
+        print("Correctly annotated cells = " + str(correctly_annotated_cells))
+        print("Annotated cells = " + str(annotated_cells))
+        print("Cell number = " + str(cell_number))
 
     def evaluate_column_type_annotation(self):
         """
-        Evaluate column type annotation (CTA) task.
+        Evaluate column type annotation (CTA) task
         """
         # Get class checked data for tables from T2Dv2 dataset
         checked_data = pd.DataFrame(pd.read_csv(EvaluationPath.T2DV2 + EvaluationPath.T2DV2_CLASS_CHECKED,
                                                 sep=",", header=None, index_col=False))
-        perfect_annotations, okay_annotations, wrong_annotations, all_annotations, rows, column_indices = 0, 0, 0, 0, [], {}
+        perfect, okay, total, rows, column_indices = 0, 0, 0, [], {}
         for key, items in checked_data.items():
             if key == 0:
                 for i in range(len(items)):
@@ -132,10 +101,10 @@ class T2Dv2TableEvaluation(TableEvaluation):
                             for row, column_index in column_indices.items():
                                 for k in range(len(self.table.columns)):
                                     if i == row and column_index == k and self.table.columns[k].annotation is not None:
-                                        all_annotations += 1
+                                        total += 1
                                     if i == row and column_index == k and items[i] and str(items[i]) != "nan":
                                         if self.table.columns[k].column_type != ColumnType.LITERAL_COLUMN and self.table.columns[k].annotation == items[i]:
-                                            perfect_annotations += 1
+                                            perfect += 1
             if key == 4:
                 for i in range(len(items)):
                     for row_index in rows:
@@ -144,25 +113,23 @@ class T2Dv2TableEvaluation(TableEvaluation):
                                 for k in range(len(self.table.columns)):
                                     if i == row and column_index == k and items[i] and str(items[i]) != "nan":
                                         if self.table.columns[k].column_type != ColumnType.LITERAL_COLUMN and self.table.columns[k].annotation in items[i].split(","):
-                                            okay_annotations += 1
+                                            okay += 1
         print("***************************************************")
         print(column_indices)
         print("target_columns = " + str(len(column_indices)))
-        print("perfect_annotations = " + str(perfect_annotations))
-        print("okay_annotations = " + str(okay_annotations))
-        print("all_annotations = " + str(all_annotations))
-        wrong_annotations = all_annotations - (perfect_annotations + okay_annotations)
-        print("wrong_annotations = " + str(wrong_annotations))
+        print("perfect_annotations = " + str(perfect))
+        print("okay_annotations = " + str(okay))
+        print("all_annotations = " + str(total))
+        wrong = total - (perfect + okay)
+        print("wrong_annotations = " + str(wrong))
         print("***************************************************")
-        # Calculate evaluations
-        self._column_type_annotation_evaluation = AdditionalEvaluation()
-        self._column_type_annotation_evaluation._average_hierarchical_score = (1 * perfect_annotations + 0.5 * okay_annotations - 1 * wrong_annotations) / len(column_indices)
-        self._column_type_annotation_evaluation._average_perfect_score = perfect_annotations / all_annotations if all_annotations != 0 else 0
+        self.column_type_annotation_evaluation.set_average_hierarchical_score((1 * perfect + 0.5 * okay - 1 * wrong) / len(column_indices) if len(column_indices) != 0 else 0)
+        self.column_type_annotation_evaluation.set_average_perfect_score(perfect / total if total != 0 else 0)
 
 
 def evaluate_t2dv2_dataset():
     """
-    Get experimental evaluation for tables from T2Dv2 dataset.
+    Get experimental evaluation for tables from T2Dv2 dataset
     """
     start_full_time = datetime.now()
     table_evaluations = []
